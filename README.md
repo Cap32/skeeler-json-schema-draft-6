8b1f128e13d4903c3d649150f32ec29a19ce1f6a
3went2 <webb.png@gmail.com>
2018-01-26 19:16:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# skeeler-json-schema-draft-6
+
+[WIP] JSON Schema Draft 6 plugin for [Skeeler](https://github.com/Cap32/skeeler)
+
+## Simple Example
+
+```js
+import Skeeler from 'skeeler';
+import SkeelerJSONSchemaDraftV6 from 'skeeler-json-schema-draft-6';
+
+const types = Skeeler.use('json', new SkeelerJSONSchemaDraftV6()).getTypes();
+
+const mySkeeler = new Skeeler({
+  foo: types.string.required.unique,
+  bar: types.number.index.exclusiveMinimum(0),
+  baz: types.objectId.required,
+  qux: types.array(types.string),
+});
+
+export default mySkeeler.export('json');
+```
+
+### Equals to JSON Schema v6
+
+```js
+export default {
+  properties: {
+    foo: {
+      type: 'string',
+    },
+    bar: {
+      type: 'number',
+      exclusiveMinimum: 0,
+    },
+    baz: {},
+    qux: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+    },
+  },
+  required: ['foo', 'baz'],
+};
+```
+
+## Related projects
+
+* [Skeeler](https://github.com/Cap32/skeeler)
+
+## License
+
+MIT
---
+++ test.js
@@ -0,0 +1,104 @@
+import Skeeler from 'skeeler';
+import SkeelerJSONSchemaDraftV6 from './src';
+
+describe('skeeler json schema draft 6', function () {
+	const name = 'json';
+	const types = Skeeler.use(name, new SkeelerJSONSchemaDraftV6()).getTypes();
+
+	test('string', function () {
+		const skeeler = new Skeeler({
+			foo: types.string,
+		});
+		expect(skeeler.export(name)).toEqual({
+			properties: { foo: { type: 'string' } },
+		});
+	});
+
+	test('number', function () {
+		const skeeler = new Skeeler({
+			foo: types.number,
+		});
+		expect(skeeler.export(name)).toEqual({
+			properties: { foo: { type: 'number' } },
+		});
+	});
+
+	test('boolean', function () {
+		const skeeler = new Skeeler({
+			foo: types.boolean,
+		});
+		expect(skeeler.export(name)).toEqual({
+			properties: { foo: { type: 'boolean' } },
+		});
+	});
+
+	test('null', function () {
+		const skeeler = new Skeeler({
+			foo: types.null,
+		});
+		expect(skeeler.export(name)).toEqual({
+			properties: { foo: { type: 'null' } },
+		});
+	});
+
+	test('any', function () {
+		const skeeler = new Skeeler({
+			foo: types.any,
+		});
+		expect(skeeler.export(name)).toEqual({
+			properties: { foo: { type: 'any' } },
+		});
+	});
+
+	test('array', function () {
+		const skeeler = new Skeeler({
+			foo: types.array,
+		});
+		expect(skeeler.export(name)).toEqual({
+			properties: { foo: { type: 'array' } },
+		});
+	});
+
+	test('array()', function () {
+		const skeeler = new Skeeler({
+			foo: types.array(types.string),
+		});
+		expect(skeeler.export(name)).toEqual({
+			properties: { foo: { type: 'array', items: { type: 'string' } } },
+		});
+	});
+
+	test('object', function () {
+		const skeeler = new Skeeler({
+			foo: types.object,
+		});
+		expect(skeeler.export(name)).toEqual({
+			properties: { foo: { type: 'object' } },
+		});
+	});
+
+	test('object()', function () {
+		const skeeler = new Skeeler({
+			foo: types.object({
+				bar: types.string,
+			}),
+		});
+		expect(skeeler.export(name)).toEqual({
+			properties: {
+				foo: { type: 'object', properties: { bar: { type: 'string' } } },
+			},
+		});
+	});
+
+	test('required', function () {
+		const skeeler = new Skeeler({
+			foo: types.required,
+		});
+		expect(skeeler.export(name)).toEqual({
+			properties: {
+				foo: {},
+			},
+			required: ['foo'],
+		});
+	});
+});
