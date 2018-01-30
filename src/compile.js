37362cbcded22d6e04bae856133f81a47ea61b53
3went2 <webb.png@gmail.com>
2018-01-30 17:51:16 +0800

    add `strict` and `other` option

--- src/compile.js
+++ src/compile.js
@@ -1,20 +1,33 @@
 import traverse from 'traverse';
 
-export default function compile(properties) {
-	const trav = traverse({ properties });
-	return trav.map(function () {
-		if (
-			this.key === 'required' &&
-			this.parent &&
-			this.parent.parent &&
-			this.parent.parent.parent &&
-			this.parent.parent.key === 'properties'
-		) {
-			this.remove();
-			const required = trav.get('required') || [];
-			required.push(this.parent.key);
-			trav.set('required', required);
-			this.parent.parent.parent.node.required = required;
-		}
-	});
+export default function compile(data, options = {}) {
+	const { strict = true, other } = options;
+
+	if (!strict) {
+		const trav = traverse({ properties: data });
+		data = trav.map(function (val) {
+			if (
+				!Array.isArray(val) &&
+				this.key === 'required' &&
+				this.parent &&
+				this.parent.parent &&
+				this.parent.parent.parent &&
+				this.parent.parent.key === 'properties'
+			) {
+				this.remove();
+				const required = trav.get('required') || [];
+				const { key } = this.parent;
+
+				/* istanbul ignore else */
+				if (!~required.indexOf(key)) {
+					required.push(key);
+				}
+
+				trav.set('required', required);
+				this.parent.parent.parent.node.required = required;
+			}
+		});
+	}
+
+	return { ...data, ...other };
 }
